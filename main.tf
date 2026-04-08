10ca4dc10aa2664d58325a4dc337112d7b73b55a
harish <122397227+harish4csl@users.noreply.github.com>
2026-04-08 21:00:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0f88e80871fd81e91"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "harishec2"
+    }
+}
