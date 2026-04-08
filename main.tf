c9be2329cd9127340a8c0b8fbdcee66e3ebb2212
harish <122397227+harish4csl@users.noreply.github.com>
2026-04-08 21:03:48 +0530

    Update AWS instance resource configuration

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+resource "aws_instance" "name" {
+  ami = "ami-0f559c3642608c138"
+  instance_type = "t3.micro"
     tags = {
       Name = "harishec2"
     }
